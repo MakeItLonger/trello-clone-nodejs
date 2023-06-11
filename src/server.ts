631fe339ecc362e53536e35d0870767adf378c9d
MakeItLonger <zavadiuk@gmail.com>
2023-06-11 17:51:41 +0300

    create authentication (login, register and currentUser methods)

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,26 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import mongoose from 'mongoose';
-
+import * as usersController from './controllers/users';
+import bodyParser from 'body-parser';
+import auth from './middlewares/auth';
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.send("API is working");
 });
+
+app.post('/api/users', usersController.register);
+app.post('/api/users/login', usersController.login);
+
+app.get('/api/user', auth, usersController.currentUser);
+
 
 io.on('connection', () => {
     console.log('socket.io is connected!');
@@ -20,7 +31,7 @@ mongoose.connect('mongodb://localhost:27017/trello').then(() => {
     console.log('connected to mongodb');
 
     httpServer.listen(4005, () => {
-        console.log(`API is listening on port 4001`);
+        console.log(`API is listening on port 4005`);
     })
 });
 
